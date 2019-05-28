a3671a6f13bee78dfbe1fe667a45e57d11ca3866
litiaohua <tiaohuali@qq.com>
2019-05-28 22:57:21 +0800

    no message

---
+++ src/main/java/com/lucky/demo2/Bean2.java
@@ -0,0 +1,10 @@
+package com.lucky.demo2;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("bean2")
+@Scope("prototype")
+public class Bean2 {
+
+}
